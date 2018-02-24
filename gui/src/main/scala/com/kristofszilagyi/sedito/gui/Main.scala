9ceba3c0b47bb53b8f50869ea7aa5009db7cf52f
kszilagyi <kristof.szilagyi1@gmail.com>
2018-02-24 12:30:18 +0000

    Added PaddableEditor.scala

--- gui/src/main/scala/com/kristofszilagyi/sedito/gui/Main.scala
+++ gui/src/main/scala/com/kristofszilagyi/sedito/gui/Main.scala
@@ -1,41 +1,25 @@
 package com.kristofszilagyi.sedito.gui
 
 
-import javafx.collections.ListChangeListener
 import com.kristofszilagyi.sedito.common.Warts._
 import com.sun.javafx.css.CssError
+import javafx.collections.ListChangeListener
 import javafx.stage.DirectoryChooser
-import org.fxmisc.richtext.{CodeArea, LineNumberFactory}
 import org.log4s._
-
-import scala.language.implicitConversions
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
 import scalafx.scene.control.{Menu, MenuBar, MenuItem}
 import scalafx.scene.layout.{BorderPane, HBox, Priority}
-import scalafx.scene.{Node, Scene}
 
-object SCodeArea {
-  implicit def toDelegate(sCodeArea: SCodeArea): CodeArea = sCodeArea.delegate
-  def createCustom(): SCodeArea = { //todo extract to different type?
-    val codeArea = new SCodeArea()
-    codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea))
-    codeArea.appendText("that's a lot\n of text\n so many\ntext")
-    //codeArea.setParagraphStyle(1, List("red").asJava)
-    codeArea.setParagraphBoxStyle(1, "-fx-padding: 20 0 0 0;")
-    codeArea
-  }
-}
-
-@SuppressWarnings(Array(DefaultArguments))
-final class SCodeArea(override val delegate: CodeArea = new CodeArea) extends Node(delegate)
 
 object Main extends JFXApp {
   private val logger = getLogger
   logger.info("SeDiTo GUI started")
-  val codeArea1 = SCodeArea.createCustom()
-  val codeArea2 = SCodeArea.createCustom()
+  val codeArea1 = PaddableEditor.test()
+  codeArea1.setPadding(1, 1)
+  val codeArea2 = PaddableEditor.test()
   val hbox = new HBox {
     children = Seq(codeArea1, codeArea2)
     spacing = 10
@@ -60,6 +44,7 @@ object Main extends JFXApp {
   stage = new PrimaryStage {
     maximized = true
     scene = new Scene {
+      
       discard(stylesheets += url )
       root = new BorderPane {
         top = new MenuBar {
