5f663ec1d93c75c7b633d49369fa94c17c216826
kszilagyi <kristof.szilagyi1@gmail.com>
2018-03-17 14:27:33 +0000

    Wordizer

---
+++ common/src/test/scala/com.kristofszilagyi/sedito/common/WordizerTest.scala
@@ -0,0 +1,34 @@
+package com.kristofszilagyi.sedito.common
+
+import org.scalatest.FreeSpecLike
+import org.scalatest.Matchers._
+
+final class WordizerTest extends FreeSpecLike {
+  "empty is empty" in {
+    Wordizer.toWords("") shouldBe Seq.empty
+  }
+
+  "1 word" in {
+    Wordizer.toWords("apple") shouldBe Seq("apple")
+  }
+
+  "2 words, 1 space" in {
+    Wordizer.toWords("apple cheese") shouldBe Seq("apple", "cheese")
+  }
+
+  "2 words, 2 space" in {
+    Wordizer.toWords("apple  cheese") shouldBe Seq("apple", "cheese")
+  }
+
+  "starting with space" in {
+    Wordizer.toWords(" apple cheese") shouldBe Seq("apple", "cheese")
+  }
+
+  "2 words, 1 comma" in {
+    Wordizer.toWords("apple,cheese") shouldBe Seq("apple", ",", "cheese")
+  }
+
+  "complicated scenario" in {
+    Wordizer.toWords(", ?apple; ,   cheese_to ad ; baba") shouldBe Seq(",", "?", "apple", ";", ",", "cheese_to", "ad", ";", "baba")
+  }
+}
