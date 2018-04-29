ebb02707f0f1c86039be954d3a208ebda8708354
kszilagyi <kristof.szilagyi1@gmail.com>
2018-04-29 17:56:08 +0100

    compile errors

--- aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/CandidateCtxFinder.scala
+++ aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/CandidateCtxFinder.scala
@@ -6,7 +6,7 @@ final class CandidateCtxFinder(contexts: Set[WordWithContext], contextSize: Int)
   private val tooSmalls = contexts.filter(ctx => ctx.beforeContext.length < contextSize || ctx.afterContext.length < contextSize)
   private val fixedFinder = new FixedCtxCandidateFinder(contexts -- tooSmalls, contextSize / 3)
 
-  def possibleMatches(s: String): Set[WordIndexRange] = {
+  def possibleMatches(s: WordWithContext): Set[WordWithContext] = {
     fixedFinder.possibleMatches(s) ++ tooSmalls
   }
 }
