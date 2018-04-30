4b29bf853b52de31091e7ff57b87dbcce58ca4f1
kszilagyi <kristof.szilagyi1@gmail.com>
2018-04-30 20:36:27 +0100

    Removed inefficient and error prone FixedCtxCandidateFinder

--- aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/Aligner.scala
+++ aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/Aligner.scala
@@ -79,7 +79,7 @@ object Aligner {
       WordWithContext(rightBeforeContext, rightAfterContext, word)
     }
 
-    val candidateCtxFinder = new CandidateCtxFinder(rightContexts.toSet, contextSize = contextSize)
+    val candidateCtxFinder = new CandidateCtxFinder(rightContexts.toSet)
     leftContexts.flatMap { leftWord =>
       val candidates = if (leftWord.beforeContext.length < contextSize) {
         rightContexts
--- aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/CandidateCtxFinder.scala
+++ aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/CandidateCtxFinder.scala
@@ -2,9 +2,7 @@ package com.kristofszilagyi.sedito.aligner
 
 import com.kristofszilagyi.sedito.aligner.Aligner.WordWithContext
 
-final class CandidateCtxFinder(contexts: Set[WordWithContext], contextSize: Int) {
-  private val tooSmalls = contexts.filter(ctx => ctx.beforeContext.length < contextSize || ctx.afterContext.length < contextSize)
-  private val fixedFinder = new FixedCtxCandidateFinder(contexts -- tooSmalls, contextSize / 3)
+final class CandidateCtxFinder(contexts: Set[WordWithContext]) {
   //todo mapValuesNow
   private val positionAgnosticWordWithContexts = contexts.map(c => c.positionAgnostic -> c).groupBy(_._1).map {
     case (k, v) => k -> v.map(_._2)
@@ -13,6 +11,6 @@ final class CandidateCtxFinder(contexts: Set[WordWithContext], contextSize: Int)
   def possibleMatches(s: WordWithContext): Set[WordWithContext] = {
     val exactMatches = positionAgnosticWordWithContexts.getOrElse(s.positionAgnostic, Set.empty)
     if(exactMatches.nonEmpty) exactMatches
-    else fixedFinder.possibleMatches(s) ++ tooSmalls
+    else contexts
   }
 }
