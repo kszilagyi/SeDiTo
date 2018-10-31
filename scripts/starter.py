28c5907bc6c74e64ad7732104466c77ad8520e4d
kszilagyi <kristof.szilagyi1@gmail.com>
2018-10-31 23:01:31 +0000

    Opening with warm standby works

--- scripts/starter.py
+++ scripts/starter.py
@@ -6,21 +6,27 @@ from subprocess import call
 from os.path import expanduser
 import logging
 
-logging.basicConfig(filename=expanduser('~/.sedito/starter.log'),level=logging.DEBUG)
+logging.basicConfig(filename=expanduser('~/.sedito/starter.log'),level=logging.DEBUG,
+                    format='%(asctime)-15s %(message)s')
 
 if __name__ == "__main__":
     fifo_path = expanduser("~/.sedito/startup_pipe")
     # todo setup logging to log file
-    logging.info('path is %s' % fifo_path)
+    logging.info('path is %s, parameters: %s' % (fifo_path, str(sys.argv)))
     arg_left = sys.argv[1]
     arg_right = sys.argv[2]
+
     try:
         pipe = os.open(fifo_path, os.O_WRONLY | os.O_NONBLOCK)
         try:
             os.write(pipe, base64.standard_b64encode(arg_left) + ' ' + base64.standard_b64encode(arg_right))
+            logging.info('App was already running, passed parameters.')
         finally:
             os.close(pipe)
     except Exception as e:
         logging.info('Starting app because %s(%s) occurred.' % (type(e).__name__, e.args))
-        call(['./gui/target/universal/stage/bin/gui', arg_left, arg_right])
+        dir = os.path.dirname(os.path.realpath(__file__))
+        exe = '%s/../gui/target/universal/stage/bin/gui' % dir
+        logging.info('Running %s' % exe)
+        call([exe, arg_left, arg_right])
 
--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+set -ex
 python $(dirname "$0")/starter.py $1 $2
