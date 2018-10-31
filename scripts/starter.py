01a7e51cb56edd709dc5097d8d8bac1169e4aeb7
kszilagyi <kristof.szilagyi1@gmail.com>
2018-10-31 22:33:45 +0000

    Fix logging and starter script

--- scripts/starter.py
+++ scripts/starter.py
@@ -2,12 +2,16 @@
 import os
 import sys
 import base64
-
+from subprocess import call
 from os.path import expanduser
+import logging
+
+logging.basicConfig(filename=expanduser('~/.sedito/starter.log'),level=logging.DEBUG)
+
 if __name__ == "__main__":
     fifo_path = expanduser("~/.sedito/startup_pipe")
     # todo setup logging to log file
-    print 'path is %s' % fifo_path
+    logging.info('path is %s' % fifo_path)
     arg_left = sys.argv[1]
     arg_right = sys.argv[2]
     try:
@@ -17,6 +21,6 @@ if __name__ == "__main__":
         finally:
             os.close(pipe)
     except Exception as e:
-        print 'Starting app because %s(%s) occurred.' % (type(e).__name__, e.args)
-
+        logging.info('Starting app because %s(%s) occurred.' % (type(e).__name__, e.args))
+        call(['./gui/target/universal/stage/bin/gui', arg_left, arg_right])
 
---
+++ scripts/starter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python $(dirname "$0")/starter.py $1 $2
