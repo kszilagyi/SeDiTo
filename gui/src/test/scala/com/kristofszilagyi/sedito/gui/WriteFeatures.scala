b7e9e14af5300b3b7639eaef6193e018ef3b0602
kszilagyi <kristof.szilagyi1@gmail.com>
2019-02-25 21:08:46 +0000

    WriteFeatures

---
+++ gui/src/test/scala/com/kristofszilagyi/sedito/gui/WriteFeatures.scala
@@ -0,0 +1,29 @@
+package com.kristofszilagyi.sedito.gui
+
+import java.nio.file.{Files, Paths}
+
+import com.kristofszilagyi.sedito.common.Warts.discard
+import com.kristofszilagyi.sedito.gui.TrainAndDiff.readDataSetAndMeasureMetrics
+import smile.feature.Scaler
+import smile.write
+import scala.collection.JavaConverters._
+
+object Write {
+  def writeCSV(baseFileName: String, scaler: Scaler, data: List[Pass1PathAndSamples]): Unit = {
+    val metrics = data.flatMap(_.samples.metricsWithResults.map(_.metrics))
+    val labels = data.flatMap(_.samples.metricsWithResults.map(_.matching))
+    write.csv(scaler.transform(metrics.map(_.doubles).toArray), s"$baseFileName-features.csv")
+    discard(Files.write(Paths.get(s"$baseFileName-labels.csv"), labels.map(b => if (b) "1" else "0").asJava))
+  }
+}
+
+object WriteFeatures {
+  def main(args: Array[String]) {
+    val samples = readDataSetAndMeasureMetrics()
+
+    val (_, firstScaler) = Main.loadAI()
+    val (trainingSamples, testSamples) = Train1Pass.shuffle(samples).splitAt((samples.size * Train.trainingRatio).toInt)
+    Write.writeCSV("training", firstScaler, trainingSamples)
+    Write.writeCSV("test", firstScaler, testSamples)
+  }
+}
