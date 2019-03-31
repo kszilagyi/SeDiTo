852f1e02e14f822eb8374256e6df2f48b3b8cd9f
kszilagyi <kristof.szilagyi1@gmail.com>
2019-03-31 12:49:04 +0100

    Add proper IndentationCalculator.scala

--- gui/src/main/scala/com/kristofszilagyi/sedito/gui/IndentationCalculator.scala
+++ gui/src/main/scala/com/kristofszilagyi/sedito/gui/IndentationCalculator.scala
@@ -1,16 +1,49 @@
 package com.kristofszilagyi.sedito.gui
 
-import com.kristofszilagyi.sedito.common.LineIdx
+import com.kristofszilagyi.sedito.common.{LineIdx, Warts}
+import IndentationCalculator._
 
-
-final case class IndentationOneSide(before: Seq[Int], current: Int, after: Seq[Int], lineIdx: LineIdx, lineIdxFromEnd: Int)
+final case class IndentationOneSide(indents: Seq[Int], lineIdx: LineIdx, lineIdxFromEnd: Int)
 final case class Indentation(left: IndentationOneSide, right: IndentationOneSide)
 
+object IndentationCalculator {
+  @SuppressWarnings(Array(Warts.Var))
+  private def extend(lines: Vector[String], startIdx: LineIdx, step: Int) = {
+    var result = Seq.empty[Int]
+    def add(xs: Seq[Int], x: Int) = {
+      if (step > 0) xs :+ x
+      else x +: xs
+    }
+    val range = 7
+    var i = startIdx.i
+    while (result.size < range) {
+      i += step
+      if (i < 0 || i >= lines.size) {
+        result = add(result, 0)
+      } else {
+        val line = lines(i)
+        if (!line.trim.isEmpty) {
+          result = add(result, line.prefixLength(_.isSpaceChar))
+        }
+      }
+    }
+    result
+  }
+
+}
 final class IndentationCalculator(leftLines: Vector[String], rightLines: Vector[String]) {
   def calcIndentation(leftIdx: LineIdx, rightIdx: LineIdx): Indentation = {
     Indentation(
-      IndentationOneSide(Seq(0, 0, 0, 0, 0, 0, 0), 0, Seq(0, 0, 0, 0, 0, 0, 0), leftIdx, leftLines.size - leftIdx.i - 1),
-      IndentationOneSide(Seq(0, 0, 0, 0, 0, 0, 0), 0, Seq(0, 0, 0, 0, 0, 0, 0), rightIdx, rightLines.size - rightIdx.i - 1),
+      IndentationOneSide(
+        (extend(leftLines, leftIdx, step = -1) :+ leftLines(leftIdx.i).prefixLength(_.isSpaceChar)) ++ extend(leftLines, leftIdx, step = 1),
+        leftIdx,
+        leftLines.size - leftIdx.i - 1
+      ),
+      IndentationOneSide(
+        (extend(rightLines, rightIdx, step = -1) :+ rightLines(rightIdx.i).prefixLength(_.isSpaceChar)) ++ extend(rightLines, rightIdx, step = 1),
+        rightIdx,
+        rightLines.size - rightIdx.i - 1
+      ),
     )
   }
 
--- gui/src/test/scala/com/kristofszilagyi/sedito/gui/IndentationCalculatorTest.scala
+++ gui/src/test/scala/com/kristofszilagyi/sedito/gui/IndentationCalculatorTest.scala
@@ -1,6 +1,7 @@
 package com.kristofszilagyi.sedito.gui
 
 import com.kristofszilagyi.sedito.common.LineIdx
+import com.kristofszilagyi.sedito.common.Warts._
 import org.scalatest.FreeSpecLike
 import org.scalatest.Matchers._
 
@@ -12,8 +13,96 @@ final class IndentationCalculatorTest extends FreeSpecLike {
     val rightIdx = LineIdx(15)
     val indent = new IndentationCalculator(Vector.fill(leftLines)("import"), Vector.fill(rightLines)("import")).calcIndentation(leftIdx, rightIdx)
     indent shouldBe Indentation(
-      IndentationOneSide(Seq.fill(7)(0), 0, Seq.fill(7)(0), leftIdx, 19),
-      IndentationOneSide(Seq.fill(7)(0), 0, Seq.fill(7)(0), rightIdx, 24),
+      IndentationOneSide(Seq.fill(15)(0), leftIdx, 19),
+      IndentationOneSide(Seq.fill(15)(0), rightIdx, 24),
     )
+  }
+
+  "all 0 beginning" in {
+    val leftLines = 30
+    val rightLines = 40
+    val leftIdx = LineIdx(0)
+    val rightIdx = LineIdx(1)
+    val indent = new IndentationCalculator(Vector.fill(leftLines)(" import"), Vector.fill(rightLines)(" import")).calcIndentation(leftIdx, rightIdx)
+    indent shouldBe Indentation(
+      IndentationOneSide(Seq.fill(7)(0) ++ Seq.fill(8)(1), leftIdx, 29),
+      IndentationOneSide(Seq.fill(6)(0) ++ Seq.fill(9)(1), rightIdx, 38),
+    )
+  }
+
+  "all 0 end" in {
+    val leftLines = 30
+    val rightLines = 40
+    val leftIdx = LineIdx(29)
+    val rightIdx = LineIdx(38)
+    val indent = new IndentationCalculator(Vector.fill(leftLines)(" import"), Vector.fill(rightLines)(" import")).calcIndentation(leftIdx, rightIdx)
+    indent shouldBe Indentation(
+      IndentationOneSide(Seq.fill(8)(1) ++ Seq.fill(7)(0), leftIdx, 0),
+      IndentationOneSide(Seq.fill(9)(1) ++ Seq.fill(6)(0), rightIdx, 1),
+    )
+  }
+
+  private val leftLines = """import scala.io.Source0
+                        |import com.kristofszilagyi.sedito.common.TypeSafeEqualsOps._1
+                        |import com.kristofszilagyi.sedito.common.Warts2
+                        |import com.kristofszilagyi.sedito.common.Warts._3
+                        |import com.kristofszilagyi.sedito.gui.Analyzer.load4
+                        |import smile.plot5
+                        |
+                        |object Analyzer {7
+                        |  final case class ResultToAnalyze(predicted: Seq[Double], expected: Seq[Int]) {8
+                        |    def predictedArray: Array[Int] = predicted.map(_.round.toInt).toArray9
+                        |
+                        |    def misPredicted: Seq[(Double, Int)] = {11
+                        |      predicted.zip(expected).filter{ case (pred, exp) =>12
+                        |        val predRounded = pred.round.toInt13
+                        |        predRounded !=== exp14
+                        |      }15
+                        |    }16
+                        |  }17
+                        |
+                        |  @SuppressWarnings(Array(Warts.ToString))19
+                        |  def load(baseFilename: String): ResultToAnalyze = {20
+                        |    val predictedLines = Source.fromFile(Write.predictedPath(baseFilename).toString).getLines().toVector21
+                        |
+                        |    ResultToAnalyze(predicted = predictedLines.map(_.toDouble), expected = expectedLines.map(_.toInt))23
+                        |  }24
+                        |
+                        |}26""".stripMargin.lines.toVector
+
+  private val rightLines = """import scala.io.Source0
+                            |import com.kristofszilagyi.sedito.common.TypeSafeEqualsOps._1
+                            |import com.kristofszilagyi.sedito.common.Warts2
+                            |import com.kristofszilagyi.sedito.common.Warts._3
+                            |import com.kristofszilagyi.sedito.gui.Analyzer.load4
+                            |import smile.plot5
+                            |
+                            |object Analyzer {7
+                            |  final case class ResultToAnalyze(predicted: Seq[Double], expected: Seq[Int]) {8
+                            |    def predictedArray: Array[Int] = predicted.map(_.round.toInt).toArray9
+                            |    def expectedArray: Array[Int] = expected.toArray10
+                            |    def misPredicted: Seq[(Double, Int)] = {11
+                            |      predicted.zip(expected).filter{ case (pred, exp) =>12
+                            |
+                            |        predRounded !=== exp14
+                            |      }15
+                            |    }16
+                            |  }17
+                            |
+                            |  @SuppressWarnings(Array(Warts.ToString))19
+                            |  def load(baseFilename: String): ResultToAnalyze = {20
+                            |
+                            |    val expectedLines = Source.fromFile(Write.expectedPath(baseFilename).toString).getLines().toVector22
+                            |    ResultToAnalyze(predicted = predictedLines.map(_.toDouble), expected = expectedLines.map(_.toInt))23
+                            |  }24
+                            |
+                            |}26""".stripMargin.lines.toVector
+
+  "middle non-trivial" in {
+    val leftIdx = LineIdx(15)
+    val rightIdx = LineIdx(17)
+    val indent = new IndentationCalculator(leftLines, rightLines).calcIndentation(leftIdx, rightIdx)
+    discard(indent.left.indents shouldBe Seq(0, 2, 4, 4, 6, 8, 8, 6, 4, 2, 2, 2, 4, 4, 2))
+    indent.right.indents shouldBe Seq(4, 4, 4, 6, 8, 6, 4, 2, 2, 2, 4, 4, 2, 0, 0)
   }
 }
