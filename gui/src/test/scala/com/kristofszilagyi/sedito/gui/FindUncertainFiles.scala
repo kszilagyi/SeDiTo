cb11a807521ccfc7f9ead0d449ceec0844185e24
kszilagyi <kristof.szilagyi1@gmail.com>
2019-04-20 22:09:29 +0100

    Writing uncertain.txt now, overwriting header

--- gui/src/test/scala/com/kristofszilagyi/sedito/gui/FindUncertainFiles.scala
+++ gui/src/test/scala/com/kristofszilagyi/sedito/gui/FindUncertainFiles.scala
@@ -1,8 +1,10 @@
 package com.kristofszilagyi.sedito.gui
 
-import java.io.File
+import java.io.{File, PrintWriter}
 
-import com.kristofszilagyi.sedito.aligner.Pass1Aligner
+import com.kristofszilagyi.sedito.aligner.{Pass1Aligner, Pass1FeatureCalculator}
+import com.kristofszilagyi.sedito.common.FullText
+import com.kristofszilagyi.sedito.common.TypeSafeEqualsOps._
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.diff.DiffEntry.ChangeType
 import org.eclipse.jgit.lib.{ObjectId, ObjectReader}
@@ -10,7 +12,6 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import org.eclipse.jgit.treewalk.CanonicalTreeParser
 
 import scala.collection.JavaConverters._
-import com.kristofszilagyi.sedito.common.TypeSafeEqualsOps._
 
 object FindUncertainFiles {
   private def idToFileString(reader: ObjectReader, objectId: ObjectId) = {
@@ -28,9 +29,8 @@ object FindUncertainFiles {
     val commits = git.log().all().call().iterator().asScala.toVector
     val (classifier, scaler) = Main.loadAI()
     val aligner = new Pass1Aligner(classifier, scaler)
-
+    val writer = new PrintWriter(new File("uncertain.txt"))
     commits.foreach { commit =>
-      println(commit.getId)
       val currentTree = new CanonicalTreeParser()
       currentTree.resetRoot(reader, commit.getTree)
 
@@ -39,10 +39,20 @@ object FindUncertainFiles {
 
       git.diff().setNewTree(currentTree).setOldTree(parentTree).call().asScala.foreach{ diff =>
         if (diff.getChangeType ==== ChangeType.MODIFY) {
-          println(diff.getNewPath)
-          println(idToFileString(reader, diff.getOldId.toObjectId))
-          println(idToFileString(reader, diff.getNewId.toObjectId))
-          aligner
+          val left = FullText(idToFileString(reader, diff.getOldId.toObjectId))
+          val right = FullText(idToFileString(reader, diff.getNewId.toObjectId))
+          val metrics = Pass1FeatureCalculator.calcAlignerFeatures(left, right)
+          val predictions = aligner.findPotentialMatches(metrics, minP = 0.0)
+          val uncertain = predictions.count(p => p.probability > 0.01 && p.probability < 0.99)
+          val quiteUncertain = predictions.count(p => p.probability > 0.1 && p.probability < 0.9)
+          val veryUncertain = predictions.count(p => p.probability > 0.2 && p.probability < 0.8)
+          val uncertainDensity = uncertain.toDouble / predictions.size.toDouble
+          val quiteUncertainDensity = quiteUncertain.toDouble / predictions.size.toDouble
+          val veryUncertainDensity = veryUncertain.toDouble / predictions.size.toDouble
+          val resultString = s"${commit.getId.name}/${diff.getNewPath};$uncertain;$quiteUncertain;$veryUncertain;$uncertainDensity;" +
+            s"$quiteUncertainDensity;$veryUncertainDensity\n"
+          writer.append(resultString)
+          writer.flush()
         }
 
       }
