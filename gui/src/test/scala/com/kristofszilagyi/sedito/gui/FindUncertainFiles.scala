12da4cea321fa59fee867e4ed9e0f9df60d918cc
kszilagyi <kristof.szilagyi1@gmail.com>
2019-04-22 15:08:03 +0100

    find uncertains in intellj source

--- gui/src/test/scala/com/kristofszilagyi/sedito/gui/FindUncertainFiles.scala
+++ gui/src/test/scala/com/kristofszilagyi/sedito/gui/FindUncertainFiles.scala
@@ -3,8 +3,9 @@ package com.kristofszilagyi.sedito.gui
 import java.io.{File, FileWriter}
 
 import com.kristofszilagyi.sedito.aligner.{Pass1Aligner, Pass1FeatureCalculator}
-import com.kristofszilagyi.sedito.common.FullText
+import com.kristofszilagyi.sedito.common.{FullText}
 import com.kristofszilagyi.sedito.common.TypeSafeEqualsOps._
+import com.kristofszilagyi.sedito.common.Warts._
 import com.kristofszilagyi.sedito.common.utils.Control._
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.diff.DiffEntry.ChangeType
@@ -24,42 +25,39 @@ object FindUncertainFiles {
     new String(data, "utf-8")
   }
   def main(args: Array[String]): Unit = {
-    val repoLocation = "."
+    val repoLocation = "../intellij-scala"
     val builder = new FileRepositoryBuilder
-    val repository = builder.setWorkTree(new File(repoLocation).getAbsoluteFile).readEnvironment.findGitDir
+    val repository = builder.setWorkTree(new File(repoLocation).getAbsoluteFile)
       .build
-
     val git = new Git(repository)
     val reader = repository.newObjectReader()
     val commits = git.log().all().call().iterator().asScala.toVector
     val (classifier, scaler) = Main.loadAI()
     val aligner = new Pass1Aligner(classifier, scaler)
-    val file = new File("uncertain.txt")
+    val file = new File("uncertain_intellij.csv")
     val finishedCommitAndFilename = using(Source.fromFile(file)){_.getLines().map(_.split(";").head).toSet}
-    val blacklistedFilenames = Set("whole_results.txt", "results.txt", "aligner/src/main/scala/com/kristofszilagyi/sedito/aligner/HardcodedNeuralNetwork.java",
-    "aligner/src/main/resources/model.model", "linear_regression.model", "common/src/test/resources/algorithm_tests/full_tests/grid_py/alignment.json",
-    "common/src/test/resources/algorithm_tests/full_tests/18/alignment.json", "common/src/test/resources/algorithm_tests/full_tests/24/alignment.json")
+    val blacklistedFilenames = Set.empty[String]
 
-    val blacklistedFragments = Set("alignment.json")
+    val blacklistedFragments = Set.empty[String]
     val writer = new FileWriter(file, true)
 
     commits.foreach { commit =>
       val currentTree = new CanonicalTreeParser()
-      currentTree.resetRoot(reader, commit.getTree)
+      discard(currentTree.resetRoot(reader, commit.getTree))
 
       val parentTree = new CanonicalTreeParser()
-      parentTree.reset(reader, commit.getParent(0).getTree)
+      discard(parentTree.reset(reader, commit.getParent(0).getTree))
 
       git.diff().setNewTree(currentTree).setOldTree(parentTree).call().asScala.foreach{ diff =>
         if (diff.getChangeType ==== ChangeType.MODIFY) {
           val commitAndFilename = s"${commit.getId.name}/${diff.getNewPath}"
           val noFragments = !blacklistedFragments.exists(fragment => diff.getNewPath.contains(fragment))
           if (!finishedCommitAndFilename.contains(commitAndFilename) && !blacklistedFilenames.contains(diff.getNewPath)
-              && noFragments){
+              && noFragments && (diff.getNewPath.endsWith(".java") || diff.getNewPath.endsWith(".scala"))){
             logger.info(s"Doing $commitAndFilename")
             val left = FullText(idToFileString(reader, diff.getOldId.toObjectId))
             val right = FullText(idToFileString(reader, diff.getNewId.toObjectId))
-            if (left.s.length < 40000 && right.s.length < 40000) {
+            if (left.s.length < 20000 && right.s.length < 20000) {
 
               val metrics = Pass1FeatureCalculator.calcAlignerFeatures(left, right)
               val predictions = aligner.findPotentialMatches(metrics, minP = 0.0)
@@ -71,7 +69,7 @@ object FindUncertainFiles {
               val veryUncertainDensity = veryUncertain.toDouble / predictions.size.toDouble
               val resultString = s"$commitAndFilename;$uncertain;$quiteUncertain;$veryUncertain;$uncertainDensity;" +
                 s"$quiteUncertainDensity;$veryUncertainDensity\n"
-              writer.append(resultString)
+              discard(writer.append(resultString))
               writer.flush()
             } else {
               logger.info(s"Skipping file as it's too big: $commitAndFilename")
